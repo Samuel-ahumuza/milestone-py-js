88a6c57353369d3015117fbaeb5bceb0ea2cd6b3
Samuel Ahumuza <ahumuzas515@gmail.com>
2025-09-26 21:33:45 +0300

    conditions.js

--- conditions.js
+++ conditions.js
@@ -22,3 +22,35 @@ if (finalMarks < 50){
 else{
     console.log("You have successfully passed");
 }
+
+let weekDayNumber= 20;
+// 1="Monday", 2="Tuesday", 3="Wednesday",.....7="Sunday"
+console.log(weekDayNumber);
+switch(weekDayNumber){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+            case 3:
+        console.log("Wednesday");
+        break;
+            case 4:
+        console.log("Thursday");
+        break;
+            case 5:
+        console.log("Friday");
+        break;
+            case 6:
+        console.log("Saturday");
+        break;
+            case 7:
+        console.log("Sunday");
+        break;
+        default:
+            console.log("Out of range");
+
+}
+
+
